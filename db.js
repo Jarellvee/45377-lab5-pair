c59352dd2548ac97f5d1491b9fc17a1ce3727850
Jarell Vicencio <mjnvicencio@gmail.com>
2025-10-18 14:56:35 -0700

    docs

--- db.js
+++ db.js
@@ -1,13 +1,23 @@
 import 'dotenv/config.js';
 import mysql from 'mysql2/promise';
-console.log("🟢 db.js starting…");
 
+/**
+ * Database class that handles connection and mysql queries
+ */
 class Database {
 
+    /**
+     * Initializes the Database instance.
+     * */
     constructor() {
         this.connection = null;
     }
 
+    /**
+     * Sets up the database and patient table if they do not exist.
+     * Checks if theres a connection, if not make one
+     * Checks if db exists, if not create it, same for patient table
+     * */
     async setup() {
         if (!this.connection) {
             this.connection = await mysql.createConnection({
@@ -19,8 +29,8 @@ class Database {
             console.log("connected to MySQL server");
         }
 
-        await this.connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
-        await this.connection.query(`USE ${process.env.DB_NAME}`);
+        await this.connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`); // check for db, create if not exists
+        await this.connection.query(`USE ${process.env.DB_NAME}`); // use that db after making sure it exists
 
         await this.connection.query(`
         CREATE TABLE IF NOT EXISTS patient (
@@ -28,12 +38,17 @@ class Database {
             name VARCHAR(100) NOT NULL,
             dateOfBirth DATE NOT NULL
         ) ENGINE=InnoDB;
-    `);
+    `); // check for patient table, create if not exists
         console.log("Database and patient table ready");
     }
 
 
-
+    /**
+     *  Executes a passed sql query with params
+     * @param {*} sql - The SQL query string.
+     * @param {*} params - The parameters for the query.
+     * @returns The result of the query.
+     */
     async query(sql, params = []) {
         try {
             const [rows] = await this.connection.execute(sql, params); // paramterized query, so safe from injection
@@ -45,6 +60,10 @@ class Database {
     }
 }
 
+/**
+ * DbRunner class to run the database setup
+ * Instantiates the Database class and calls the setup method to fully initialize the database
+ */
 class DbRunner {
     constructor() {
         this.db = new Database();
